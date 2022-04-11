1acac25e15433956738de135721c93e920d0c226
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-11 13:28:26 -0400

    commit tree 1b29ca900a528a60646bbc856b4a5618eb1852d4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const newP = document.querySelector('#text');
+// console.log(newP)
+document.addEventListener('DOMContentLoaded', loaded);
+
+function loaded () {
+    document.querySelector('p').textContent = 'This is really cool!';
+}
+
+console.log(loaded)
